19919ac3227c1c31758ddf0ff06d8dfd09a1cecc
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-02-28 16:33:25 +0200

    upd

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,10 +1,18 @@
-function UserList({ person }) {
+function UserList({ person, setPerson }) {
+    // const delHandler = () => {
+    //     setPerson((p) => p.filter((p) => p.id !== person.id));
+    // };
+    const delHandler = () => {
+        setPerson((p) => p.filter((s) => p.id !== person.id));
+    };
+
     return (
         <ul>
             {person.map((e) => (
                 <li key={e.id} style={{ color: e.color }}>
                     {' '}
-                    {e.name}{' '}
+                    {e.name}
+                    <button onClick={delHandler}>DEL</button>
                 </li>
             ))}
         </ul>
